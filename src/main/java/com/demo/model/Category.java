e0ce1bf5d1fb0753a4531a61dcbd5d1791905eda
John Smith <example@email.com>
2020-06-25 12:07:34 +0700

    hello

---
+++ src/main/java/com/demo/model/Category.java
@@ -0,0 +1,6 @@
+package com.demo.model;
+
+public class Category {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/demo/model/Fruit.java
@@ -0,0 +1,11 @@
+package com.demo.model;
+
+public class Fruit {
+    private int id;
+    private String name;
+    private String description;
+    private String price;
+    private String unit;
+    private String origin;
+    private String image;
+}
