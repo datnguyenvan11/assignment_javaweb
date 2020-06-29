da44aa5a713f914059ff0eea6fb336564305e82b
anhntth1807026 <anhntth1807026@fpt.edu.vn>
2020-06-29 10:16:19 +0700

    add update and delete cate and fruit

--- src/main/java/com/demo/dao/FruitDAO.java
+++ src/main/java/com/demo/dao/FruitDAO.java
@@ -12,12 +12,49 @@ public class FruitDAO {
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+//    FRUIT CRUD
+
     public void insertFruit(Fruit fruit){
         entityManager.getTransaction().begin();
         entityManager.persist(fruit);
         entityManager.getTransaction().commit();
         entityManager.close();
     }
+
+    public List<Fruit> getAllFruit(){
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<Fruit> list = entityManager.createQuery("select c from Fruit c", Fruit.class).getResultList();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return list;
+    }
+
+    public void updateFruit(Fruit fruit) {
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Fruit entityFruit = entityManager.find(Fruit.class, fruit.getId());
+        entityFruit.setName(fruit.getName());
+        entityFruit.setDescription(fruit.getDescription());
+        entityFruit.setPrice(fruit.getPrice());
+        entityFruit.setUnit(fruit.getUnit());
+        entityFruit.setOrigin(fruit.getOrigin());
+        entityFruit.setThumbnail(fruit.getThumbnail());
+        entityFruit.setCategoryId(fruit.getCategoryId());
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    public void deleteFruit(int id) {
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Fruit entityFruit = entityManager.find(Fruit.class, id);
+        entityManager.remove(entityFruit);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+//    CATEGORY CRUD
 
     public void insertCategory(Category category){
         entityManager.getTransaction().begin();
@@ -35,12 +72,22 @@ public class FruitDAO {
         return list;
     }
 
-    public List<Fruit> getAllFruit(){
+    public void updateCategory(Category category) {
         entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
-        List<Fruit> list = entityManager.createQuery("select c from Fruit c", Fruit.class).getResultList();
+        Category entityCategory = entityManager.find(Category.class, category.getId());
+        entityCategory.setId(category.getId());
+        entityCategory.setName(category.getName());
         entityManager.getTransaction().commit();
         entityManager.close();
-        return list;
+    }
+
+    public void deleteCategory(int id) {
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Category entityCategory = entityManager.find(Category.class, id);
+        entityManager.remove(entityCategory);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 }
